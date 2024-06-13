c94afe0e918f4da03102260d72edebfce10fe7c1
惠文华 <huiwenhua@huiwenhuadeMacBook-Pro.local>
2024-06-13 12:09:12 +0800

    feat: layout

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,7 @@
+import { FooterStyle } from "./FooterStyle"
+
+export const Footer = () => {
+    return (
+        <FooterStyle>技术支持单位：北京国科蓝海科技有限公司</FooterStyle>
+    )
+}
---
+++ src/layout/FooterStyle.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterStyle  = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    font-size: 24px;
+    text-align: center;
+    color: #fff;
+`

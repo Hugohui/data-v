4d739157aa8e23687e73018d1980d6962292a12a
惠文华 <huiwenhua@huiwenhuadeMacBook-Pro.local>
2024-07-10 16:36:22 +0800

    feat: env info

--- src/pages/DataV/components/EnvInfo.tsx
+++ src/pages/DataV/components/EnvInfo.tsx
@@ -5,12 +5,20 @@ import { useState } from "react"
 import { useIntervalRequest } from "@/hooks/useIntervalRequest"
 
 export const EnvInfo = () => {
-    const [data, setData] = useState({})
+    const [data, setData] = useState<any>({})
+
+    const formatData = (data: any) => {
+        const res: any = {}
+        data?.forEach((item:any) => {
+            res[item.pointName] = item.pointValue
+        })
+        return res
+    }
 
     const getData = () => {
         getEnvironmentalMonitoring().then((res:any) => {
-            if (res.data) {
-                setData(res.data)
+            if (res.data && res.data[0] && res.data[0].resultData) {
+                setData(formatData(res.data[0].resultData))
             }
         })
     }
@@ -19,12 +27,12 @@ export const EnvInfo = () => {
 
     return (
         <EnvInfoStyle>
-            <EnvInfoItem value="28" label="温度" icon="temperature" unit="°C"></EnvInfoItem>
-            <EnvInfoItem value="46" label="湿度" icon="humidness" unit="%"></EnvInfoItem>
-            <EnvInfoItem value="24" label="水槽温度" icon="tank-temperature" unit="°C"></EnvInfoItem>
-            <EnvInfoItem value="21" label="二氧化碳" icon="co2" unit="%"></EnvInfoItem>
-            <EnvInfoItem value="13" label="光照" icon="lux" unit="LUX"></EnvInfoItem>
-            <EnvInfoItem value="14" label="水质" icon="water-ph" unit="PH"></EnvInfoItem>
+            <EnvInfoItem value={data['温度']} label="温度" icon="temperature" unit="°C"></EnvInfoItem>
+            <EnvInfoItem value={data['湿度']} label="湿度" icon="humidness" unit="%"></EnvInfoItem>
+            <EnvInfoItem value={data['水槽温度']} label="水槽温度" icon="tank-temperature" unit="°C"></EnvInfoItem>
+            <EnvInfoItem value={data['二氧化碳']} label="二氧化碳" icon="co2" unit="%"></EnvInfoItem>
+            <EnvInfoItem value={data['光照']} label="光照" icon="lux" unit="LUX"></EnvInfoItem>
+            <EnvInfoItem value={data['水质']} label="水质" icon="water-ph" unit="PH"></EnvInfoItem>
         </EnvInfoStyle>
     )
 }

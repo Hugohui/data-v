99422c90274f0714ea8225f821ee67aaf311638a
惠文华 <huiwenhua@huiwenhuadeMacBook-Pro.local>
2024-07-15 17:39:27 +0800

    feat: REFRESH

--- src/enums/constantEnum.ts
+++ src/enums/constantEnum.ts
@@ -2,8 +2,8 @@
  * @description: 时间枚举
  */
 export enum TimeEnum {
-    // REFRESH = 1000 * 60 * 2 // 刷新接口的间隔，2分钟
-    REFRESH = 1000 * 10
+    REFRESH = 1000 * 60 * 2 // 刷新接口的间隔，2分钟
+    // REFRESH = 1000 * 10
 }
 
 /**

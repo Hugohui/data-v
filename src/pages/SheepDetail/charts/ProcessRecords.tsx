e980d1db82558603f45691ccb4916aa73cd1cbad
huiwenhua <huiwenhua@aliyun.com>
2024-09-07 22:11:48 +0800

    feat: style

--- src/pages/SheepDetail/charts/ProcessRecords.tsx
+++ src/pages/SheepDetail/charts/ProcessRecords.tsx
@@ -18,18 +18,21 @@ export const ProcessRecords = () => {
 
     return (
         <ProcessRecordsStyle>
-            <div className="timeline">
-                {
-                    records?.map((item: any, index: number) => (
-                        <div key={index} className="timeline-item">
-                            <div className={['timeline-content', index % 2 === 0 ? 'foatLeft' : 'foatRight'].join(" ")}>
-                                <p>{item?.value?.substr(0, 9)}</p>
-                                <p>{item?.name}</p>
+            {
+                records.length ? <div className="timeline">
+                    {
+                        records?.map((item: any, index: number) => (
+                            <div key={index} className="timeline-item">
+                                <div className={['timeline-content', index % 2 === 0 ? 'foatLeft' : 'foatRight'].join(" ")}>
+                                    <p>{item?.value?.substr(0, 9)}</p>
+                                    <p>{item?.name}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
-            </div>
+                        ))
+                    }
+                </div> : ''
+            }
+
         </ProcessRecordsStyle>
     )
 }
